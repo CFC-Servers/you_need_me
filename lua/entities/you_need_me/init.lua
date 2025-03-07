5a97e4c20e174a05cdd6cd9f6b2cf53d526eeb76
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-03-07 15:14:00 +0100

    Remove unused cl_init file

--- lua/entities/you_need_me/init.lua
+++ lua/entities/you_need_me/init.lua
@@ -1,7 +1,5 @@
 AddCSLuaFile( "shared.lua" )
-AddCSLuaFile( "cl_init.lua" )
 include( "shared.lua" )
-include( "cl_init.lua" )
 
 function ENT:Initialize()
     self:SetModel( "models/hunter/blocks/cube025x025x025.mdl" )
